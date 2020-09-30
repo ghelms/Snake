7bf7f2875201e947ab5cc8a0c35a35f46fd0e867
ghelms <gustav.helms@yahoo.dk>
2020-09-30 11:16:11 +0200

    hwp

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 import pygame
 
-#Intitialize game
+# Intitialize game
 pygame.init()
 
-#Create the screen
-screen = pygame.display.set_mode((800,600))
+# Create the screen
+screen = pygame.display.set_mode((450, 450))
 
-#Background
-background = 
+# Background
+background = pygame.image.load("grass.jpg")
+
+# Title and icon
+pygame.display.set_caption("Snake invaders")
+icon = pygame.image.load("icon.png")
+pygame.display.set_icon(icon)
+
+# Game loop
+running = True
+while running:
+
+    # RGB - (MAKING IT BLACK)
+    screen.fill((0, 0, 0))
+
+    # Adding background
+    screen.blit(background, (0, 0))
+
+    # Looping through events
+    for event in pygame.event.get():
+
+        # Making sure we can quit the game
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Update the screen
+    pygame.display.update()
